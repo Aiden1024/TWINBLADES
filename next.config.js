833954b89577331e7d62c72344b65fa30784bd11
Aiden1024 <edgex1024@gmail.com>
2025-04-15 15:55:03 +0800

    try build again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		unoptimized: true, // 对于静态导出需要
+	},
+	basePath: '/TWINBLADES', // 如果你的仓库名不是 username.github.io
+	assetPrefix: '/TWINBLADES/', // 同上
+
 	output: 'export',
 }
 
